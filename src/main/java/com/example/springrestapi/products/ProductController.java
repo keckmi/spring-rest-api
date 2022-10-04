3bcb42ca2b1276ea0631562a80871921f7ba0c4e
michael <michael-s-keck@web.de>
2022-10-04 10:07:11 +0200

    ProductController mit @PostMapping createProduct(@RequestBody Product product)

---
+++ src/main/java/com/example/springrestapi/products/ProductController.java
@@ -0,0 +1,30 @@
+package com.example.springrestapi.products;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    @Autowired
+    ProductRepository productRepository;
+
+    //PostMapping: diese Methode wird aufgerufen wenn wir eine POST anfrage machen?
+    //Spring soll in den Body gucken (POST Anfragen haben ja generell irgendwelche Informationen im Body)
+    //zum Beispiel in einem Formular (bei REST Schnittstellen (API) ist das im Json Format)
+    //Spring geht dann daher, guckt in den RequestBody rein und versucht das dann in ein Product,
+    //in eines unserer Java Objekte zu konvertieren
+    //und injiziert das ganze in unsere Methode hier rein
+    //und wir speichern das ganze dann. also das resultat von dieser serialisierung
+    @PostMapping("")
+    public void createProduct(@RequestBody Product product) {
+        productRepository.save(product);
+    }
+
+
+
+}
